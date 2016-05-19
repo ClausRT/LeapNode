ae426782f4c7a534d6b8183f86e4be449e22d4ba
Claus Tessmann <clausrt@outlook.com>
2016-05-19 12:45:31 -0300

    Angular Working
    I'm a idiot that forgot to call the js file v.v

--- public/js/controllers/HomeController.js
+++ public/js/controllers/HomeController.js
@@ -1,3 +1,4 @@
-angular.module('leap-node').module('HomeController', function ($scope) {
-    $scope.message = "Se estiveres lendo isso, os controllers do Angular estão corretamente configurados.";
+angular.module('leap-node').controller('HomeController', function ($scope) {
+    $scope.text = "Se estiveres lendo isso, os controllers do Angular estão corretamente configurados.";
+    
 });
--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,10 @@
-angular.module('leap-node', ['ngRoute']).config(function ($routeProvider) {
-    $routeProvider.when('/', {
+angular.module('leap-node', ['ngRoute', 'ngResource']).config(function ($interpolateProvider, $routeProvider) {
+    //$interpolateProvider.startSymbol('[[').endSymbol(']]');   //Caso HBS e Angular começarem a brigar pela guarda dos {{}}, definir novos para o Angular
+    
+    $routeProvider.when('/home', {
         templateUrl: 'partials/home.html',
         controller: 'HomeController'
     });
     
-    $routeProvider.otherwise({redirectTo: '/'});
+    $routeProvider.otherwise({redirectTo: '/home'});
 });
